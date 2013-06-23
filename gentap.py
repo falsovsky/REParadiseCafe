d32a1752cbdeff71e4a624fddce35b2be19df15b
Pedro de Oliveira <falsovsky@gmail.com>
2013-06-24 00:08:58 +0100

    inicio de escripe para gerar um tap igualzinho ao original, mas com o codigo recompilado

---
+++ gentap.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('./extra')
+import basic
+import scr
+
+def get_str(chars):
+    return [ord(c) for c in chars]
+
+def get_word(word):
+    return (word % 256, word // 256)
+
+def get_parity(data):
+    parity = 0
+    for b in data[2:]:
+        parity ^= b
+    return parity
+
+def gen_basic_loader(title):
+    header = []
+    header.extend(get_word(19))                  # Length of header block
+    header.append(0)                             # Header block marker
+    header.append(0)                             # BASIC program follows
+    header.extend(get_str(title[:10].ljust(10))) # Title padded with spaces
+    header.extend(get_word(len(basic.basic_data)))
+    header.extend(get_word(10))                  # RUN 10 after LOADing
+    #header.extend(get_word(32768))              # Nao autoloada
+    header.extend(get_word(len(basic.basic_data)))
+    header.append(get_parity(header))
+
+    data = [] 
+    data.extend(get_word(len(basic.basic_data)+2))
+    data.append(255)
+    for i in basic.basic_data:
+        data.append(i)
+    data.append(get_parity(data))
+    return header + data
+
+def gen_screen(title):
+    header = []
+    header.extend(get_word(19))
+    header.append(0)
+    header.append(3)
+    header.extend(get_str(title[:10].ljust(10)))
+    header.extend(get_word(6912))
+    header.extend(get_word(16384))
+    header.extend(get_word(0))
+    header.append(get_parity(header))
+
+    data = []
+    data.extend(get_word(len(scr.screen_data)+2))
+    data.append(255)
+    for i in scr.screen_data:
+        data.append(i)
+    data.append(get_parity(data))
+    return header + data
+
+
+tap_data = gen_basic_loader("benfica")
+tap_data.extend(gen_screen("glorioso"))
+
+with open("zbr.tap", 'wb') as f:
+    f.write(bytearray(tap_data))
+
